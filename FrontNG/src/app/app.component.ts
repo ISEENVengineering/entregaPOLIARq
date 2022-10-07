9ff1c0087c59ea366509130103a95d5201bc2468
Eric Martinez Acelas <isenv.projects@outlook.com>
2022-10-07 12:55:54 -0500

    smm

--- FrontNG/src/app/app.component.ts
+++ FrontNG/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
+
 export class AppComponent {
   title = 'FrontNG';
+
 }
--- FrontNG/src/app/app.module.ts
+++ FrontNG/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ImagesComponent } from './Components/images/images.component';
+import { MainComponent } from './Components/main/main.component';
+import { RegistrComponent } from './Components/registr/registr.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ProjectComponent } from './Components/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ImagesComponent,
+    MainComponent,
+    RegistrComponent,
+    FooterComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
